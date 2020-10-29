6f22d9668cb9c113baa25ab84ce808877b043bd5
dyy970319 <610594619@qq.com>
2020-10-29 21:38:32 +0800

    add linux-build

---
+++ linux-build/cmake-build.sh
@@ -0,0 +1,11 @@
+###
+ # @Description: =build linux library
+ # @Author: wolf-herd
+ # @Date: 2020-09-21 10:29:50
+ # @LastEditTime: 2020-10-19 20:29:21
+ # @LastEditors: wolf-herd
+### 
+#!/bin/bash
+cmake ../
+make
+
